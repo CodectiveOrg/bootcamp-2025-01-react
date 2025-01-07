c40b33c234032d2142aaef8b83969c53ca8f7865
BijanProgrammer <bijaneisapour@gmail.com>
2025-01-07 18:05:12 +0330

    [ADD] useState example

--- dream-journal/src/App.tsx
+++ dream-journal/src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <h1>Hello, friend!</h1>;
+  const [count, setCount] = useState(0);
+
+  console.log("App is called");
+
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 }
 
 export default App;
+
+/*
+  Component:
+  - is a function
+  - its name has to be PascalCase
+  - returns ReactNode
+  - gets props
+ */
