f64d962e4de8fe7989b2439f96fb0740ba59bed9
BijanProgrammer <bijaneisapour@gmail.com>
2025-01-07 18:58:09 +0330

    [ADD] basic html

--- dream-journal/src/App.tsx
+++ dream-journal/src/App.tsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import MingcuteSearchLine from "./icons/MingcuteSearchLine.tsx";
+import MingcuteMoonStarsLine from "./icons/MingcuteMoonStarsLine.tsx";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log("App is called");
-
   return (
-    <div>
-      <div>Count: {count}</div>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(count + 1)}>+</button>
+    <div className="app">
+      <header>
+        <h1>Dream Journal</h1>
+      </header>
+      <main>
+        <div className="toolbar">
+          <div className="search-box">
+            <input type="text" />
+            <MingcuteSearchLine className="icon" />
+          </div>
+          <select>
+            <option value="">All</option>
+            <option value="">Filter 1</option>
+            <option value="">Filter 2</option>
+          </select>
+          <button className="theme">
+            <MingcuteMoonStarsLine />
+          </button>
+        </div>
+        <ul className="items"></ul>
+      </main>
+      <footer>
+        <button className="add">+</button>
+      </footer>
     </div>
   );
 }
