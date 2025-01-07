92031925f9d9a5108885273335d694ee6c50108c
BijanProgrammer <bijaneisapour@gmail.com>
2025-01-07 20:12:16 +0330

    [ADD] button styles

--- dream-journal/src/components/Button/Button.tsx
+++ dream-journal/src/components/Button/Button.tsx
@@ -1,9 +1,40 @@
 import { ComponentProps } from "react";
 
-type Props = ComponentProps<"button">;
+import clsx from "clsx";
 
-function Button({ children, ...otherProps }: Props) {
-  return <button {...otherProps}>{children}</button>;
+import styles from "./Button.module.css";
+
+type Shape = "rectangle" | "circle";
+type Size = "small" | "large";
+
+type Props = ComponentProps<"button"> & {
+  shape?: Shape;
+  size?: Size;
+  sameWidthHeight?: boolean;
+};
+
+function Button({
+  children,
+  className,
+  shape = "rectangle",
+  size = "small",
+  sameWidthHeight = false,
+  ...otherProps
+}: Props) {
+  return (
+    <button
+      className={clsx(
+        styles.button,
+        styles[shape],
+        styles[size],
+        sameWidthHeight && styles["same-width-height"],
+        className,
+      )}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- dream-journal/src/components/Footer/Footer.tsx
+++ dream-journal/src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ import Button from "../Button/Button.tsx";
 function Footer() {
   return (
     <footer>
-      <Button className="add">+</Button>
+      <Button className="add" sameWidthHeight>
+        +
+      </Button>
     </footer>
   );
 }
